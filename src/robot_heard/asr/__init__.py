e9f9ed7171f112555e060788b2fbbb60722c4866
Yuxin Lei <1785386821@qq.com>
2026-09-16 20:45:23 +0800

    feat: export S4 batch API

--- src/robot_heard/asr/__init__.py
+++ src/robot_heard/asr/__init__.py
@@ -1,1 +1,24 @@
 """ASR package."""
+
+from .base import ASRBackend, ASRResult
+from .batch import (
+    BatchProvenance,
+    BatchResumeError,
+    BatchSummary,
+    build_batch_provenance,
+    default_error_path,
+    run_batch,
+    run_metadata_path,
+)
+
+__all__ = [
+    "ASRBackend",
+    "ASRResult",
+    "BatchProvenance",
+    "BatchResumeError",
+    "BatchSummary",
+    "build_batch_provenance",
+    "default_error_path",
+    "run_batch",
+    "run_metadata_path",
+]
